9525fea4dc88160e4752eba6856784810e3215de
ElMobD <tracertraore@gmail.com>
2023-10-29 20:50:57 +0100

    Classe Vecteur2D terminé

--- SpaceInvaders/Vecteur2D.cs
+++ SpaceInvaders/Vecteur2D.cs
@@ -20,13 +20,29 @@ namespace SpaceInvaders
                 return Math.Sqrt((this.x*this.x)+(this.y*this.y));
             }
         }
-        public static Vecteur2D operator+(Vecteur2D v1, Vecteur2D v2)
+        public static Vecteur2D operator+(Vecteur2D v1, Vecteur2D v2) /// Addition Vectorielle
         {
             return new Vecteur2D(v1.x + v2.x, v1.y + v2.y);
         }
-        public static Vecteur2D operator -(Vecteur2D v1, Vecteur2D v2)
+        public static Vecteur2D operator-(Vecteur2D v1, Vecteur2D v2) /// Soustraction Vectorielle
         {
             return new Vecteur2D(v1.x - v2.x, v1.y - v2.y);
+        }
+        public static Vecteur2D operator-(Vecteur2D v1)
+        {
+            return new Vecteur2D(-v1.x, -v1.y);
+        }
+        public static Vecteur2D operator*(Vecteur2D v1, double mul) /// Multiplication par un scalaire à droite
+        {
+            return new Vecteur2D(mul * v1.x, mul * v1.y);
+        }
+        public static Vecteur2D operator *(double mul, Vecteur2D v1 ) /// Multiplication par un scalaire à droite
+        {
+            return new Vecteur2D(mul * v1.x, mul * v1.y);
+        }
+        public static Vecteur2D operator /(Vecteur2D v1, double div) /// Multiplication par un scalaire à droite
+        {
+            return new Vecteur2D(v1.x/div, v1.y/div);
         }
     }
 }
